2e29c573eabc00933c3d1d6ba7a4d24e2a584181
John Ferlito <johnf@inodes.org>
2016-10-16 15:08:04 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+import http from 'http';
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+import routes from './routes/index';
+import auth from './routes/auth';
+
+const app = express();
+app.server = http.createServer(app);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'stylesheets'),
+  dest: path.join(__dirname, 'public/stylesheets'),
+  prefix: '/stylesheets',
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/auth', auth);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.server.listen(process.env.PORT || 3000);
+
+/*eslint no-console: "off"*/
+console.log(`Started on port ${app.server.address().port}`);
+
+module.exports = app;
