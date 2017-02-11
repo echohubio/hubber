332748517173f26aafea4b824b30e22bea10e144
John Ferlito <johnf@inodes.org>
2017-02-11 15:38:01 +1100

    Order the finalise actions

--- app/actions/setup.js
+++ app/actions/setup.js
@@ -1,23 +1,29 @@
+import electron from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
 import { push } from 'react-router-redux';
 
-export const finalise = iot => (dispatch) => {
-  console.error(iot);
-  dispatch({
-    type: 'SETUP_SET_FINALISED',
-  });
+export const finalise = iot => dispatch => (
+  Promise.all([
+    dispatch({
+      type: 'SETUP_SET_FINALISED',
+    }),
 
-  dispatch(push('/'));
-
-  dispatch({
-    type: 'PLUGINS_INITIALISE',
-    plugins: [
-      {
-        packagePath: 'hubber-iot',
-        ...iot,
-      },
-      'hubber-plugins',
-    ],
-  });
-};
+    dispatch({
+      type: 'PLUGINS_INITIALISE',
+      plugins: [
+        {
+          packagePath: 'hubber-iot',
+          ...iot,
+        },
+        'hubber-plugins',
+      ],
+    }),
+  ])
+  .then(() => {
+    dispatch(push('/'));
+  })
+  .then(() => {
+    electron.remote.getCurrentWindow().reload();
+  })
+);
 
 export const dummy = 'foo';
